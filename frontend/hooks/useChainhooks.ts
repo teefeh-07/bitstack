99c1581f302d437f83b001a2a087ebe93bc90241
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:11:53 +0100

    feat: add hook statements to useChainhooks.ts

--- frontend/hooks/useChainhooks.ts
+++ frontend/hooks/useChainhooks.ts
@@ -1,3 +1,4 @@
 import { useState, useEffect } from 'react';
 // specific imports handled locally
 import { ChainhooksClient } from '@hirosystems/chainhooks-client';
+export function useChainhooks() {
