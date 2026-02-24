ecefc01e3f048bbcb4c4245639b6b340251ce7fe
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:13:23 +0100

    feat: add hook statements to useNetwork.ts

--- frontend/hooks/useNetwork.ts
+++ frontend/hooks/useNetwork.ts
@@ -4,3 +4,4 @@ export function useNetwork() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  useEffect(() => {
