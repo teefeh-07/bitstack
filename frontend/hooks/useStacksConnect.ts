c113f695ce9df275b0776b3ee6c403ae1059a5c8
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:11:19 +0100

    feat: add hook statements to useStacksConnect.ts

--- frontend/hooks/useStacksConnect.ts
+++ frontend/hooks/useStacksConnect.ts
@@ -3,3 +3,4 @@ import { showConnect } from '@stacks/connect';
 export function useStacksConnect() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
