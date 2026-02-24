7cdd9f4abd5b5a2e599b81fc6487165fea479ca7
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:15:16 +0100

    feat: add component layout to WalletButton.tsx

--- frontend/components/WalletButton.tsx
+++ frontend/components/WalletButton.tsx
@@ -5,3 +5,4 @@ export default function WalletButton(props: any) {
     <div className="component-container">
       <h2>WalletButton Component</h2>
       <p>This component is part of the micro-commit project.</p>
+    </div>
