a1d970ae4737da45f22756b631e4a7607acc11e1
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:15:41 +0100

    feat: add component layout to ChainhooksPanel.tsx

--- frontend/components/ChainhooksPanel.tsx
+++ frontend/components/ChainhooksPanel.tsx
@@ -4,3 +4,4 @@ export default function ChainhooksPanel(props: any) {
   return (
     <div className="component-container">
       <h2>ChainhooksPanel Component</h2>
+      <p>This component is part of the micro-commit project.</p>
