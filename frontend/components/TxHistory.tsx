76e2b6e01adbdd6c38670fe4ca61345fd9353875
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:16:09 +0100

    feat: add component layout to TxHistory.tsx

--- frontend/components/TxHistory.tsx
+++ frontend/components/TxHistory.tsx
@@ -3,3 +3,4 @@ import styles from './TxHistory.module.css';
 export default function TxHistory(props: any) {
   return (
     <div className="component-container">
+      <h2>TxHistory Component</h2>
