72118f8cc748961bc7ac5390bfd6be3a8ae8df88
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:14:21 +0100

    feat: add component layout to Header.tsx

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -4,3 +4,4 @@ export default function Header(props: any) {
   return (
     <div className="component-container">
       <h2>Header Component</h2>
+      <p>This component is part of the micro-commit project.</p>
