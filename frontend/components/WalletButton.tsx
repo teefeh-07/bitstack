bece172f2b0fd7d2bf9cda2a91857e0f4a42770c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-24 10:15:13 +0100

    feat: add component layout to WalletButton.tsx

--- frontend/components/WalletButton.tsx
+++ frontend/components/WalletButton.tsx
@@ -3,3 +3,4 @@ import styles from './WalletButton.module.css';
 export default function WalletButton(props: any) {
   return (
     <div className="component-container">
+      <h2>WalletButton Component</h2>
